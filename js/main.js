e6b97e041a2521452cbb620ab8740719177c7727
hyeri10277 <69783620+hyeri10277@users.noreply.github.com>
2020-08-26 16:03:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -392,6 +392,60 @@ $(function () { /// jQB ///////////////////
         });
     });
 
+    
+    
+    
+    
+  // 인스타그램 불러오기
+  //if(!$('.instagram').length) { return; }
 
+        var token = "IGQVJVNEdXMDY2ejVqRmtIOU5aMXRrWURvU054bDRLMFVtM1ZAGb0thQnc3cmJfMHVFRUJOaWl1clpmdnBOcnRQTEJvSkczVWV5VE9LMU9sRi03TXZACRzRnT3pqRXVUd3dRaTFubVJpekxJNEtEbklfeQZDZD";
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            cache: false,
+            url: "https://graph.instagram.com/me/media?access_token=" + token + "&fields=id,caption,media_type,media_url,thumbnail_url,permalink",
+            success: function(response) {
+                console.log(response);
+                if (response.data != undefined && response.data.length > 0) {
+                    for (i = 0; i < 8; i++) {
+                        if (response.data[i]) {
+                            var item = response.data[i];
+                            var image_url = "";
+                            var post = "";
+
+                            if (item.media_type === "VIDEO") {
+                                image_url = item.thumbnail_url;
+                            } else {
+                                image_url = item.media_url;
+                            }
+
+                            post += '<div class="instagram_item instagram_item' + i + '">';
+                            post += '<a href="' + item.permalink + '" target="_blank" rel="noopener noreferrer" style="background-image: url(' + image_url + ');">';
+                            post += '<p>' + item.caption + '</p>';
+                            post += '</a>';
+                            post += '</div>';
+
+                            $('#instagram').append(post);
+                        } else {
+                            // if no curent item
+                            show_fallback('#insta-item-' + i)
+                        }
+                    }
+                } else {
+                    // if api error
+                    show_fallback('.instagram-item')
+                }
+            },
+            error: function() {
+                // if http error
+                show_fallback('.instagram-item')
+            }
+
+        });
+
+        function show_fallback(el) {
+            $(el).addClass('loaded fallback');
+        }
 
 }); /////////// jQB //////////////////////////
